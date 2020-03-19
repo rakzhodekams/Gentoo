4e164b8a5041e7f554d37e93d8308301c2af66c3
rakzhodekams <i00nsu@cryptolab.net>
2020-03-19 03:15:17 +0000

    Ficheiros para gravar a actividade gráfica usando o recordmydesktop

---
+++ home/user/bin/record_mon1.sh
@@ -0,0 +1,1 @@
+recordmydesktop --display=:0.0 --width=1600 height=900 -x=0 -y=0 --fps=15 -o /home/$USER/records/desktopVideo
---
+++ home/user/bin/record_mon2.sh
@@ -0,0 +1,1 @@
+recordmydesktop --display=$DISPLAY --width=1440 height=900 -x=1600 -y=0 --fps=15 -o /home/$USER/records/desktopVideo
